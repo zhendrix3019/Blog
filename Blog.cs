e0a6df00b3a7bdb8d613fed1f348394dd7b5e707
zhendrix3019 <114441063+zhendrix3019@users.noreply.github.com>
2023-04-03 13:41:55 -0500

    Add files via upload

---
+++ Blog.cs
@@ -0,0 +1,8 @@
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Name { get; set; }
+
+    public List<Post> Posts { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+Console.WriteLine("Hello World!");
+
+try
+{
+
+    // Create and save a new Blog
+    Console.Write("Enter a name for a new Blog: ");
+    var name = Console.ReadLine();
+
+    var blog = new Blog { Name = name };
+
+    var db = new BloggingContext();
+    db.AddBlog(blog);
+    logger.Info("Blog added - {name}", name);
+
+    // Display all Blogs from the database
+    var query = db.Blogs.OrderBy(b => b.Name);
+
+    Console.WriteLine("All blogs in the database:");
+    foreach (var item in query)
+    {
+        Console.WriteLine(item.Name);
+    }
+}
+catch (Exception ex)
+{
+    logger.Error(ex.Message);
+}
